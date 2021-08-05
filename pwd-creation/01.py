c2119a182ca9f16ffe879decdbd4cab9545859fb
giftson solomon <giftsonsolomon@icloud.com>
2021-08-05 10:03:34 +0530

    This is a first pwd creation file

---
+++ pwd-creation/01.py
@@ -0,0 +1,16 @@
+i# password creation
+a={"abcdefghijklmnopqrstuvwxyz"}
+b=("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+c=("0123456789")
+d=("~!@#$%^&*()_-+={}[];:'")
+
+print(bool(" "))
+
+#e=(print(a), print(b), print(c), print(d))
+print(a)
+
+
+#print(y[1])
+#print(x[1])
+#print(z[2])
+#print(a[6])
