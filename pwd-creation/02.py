61432a1f1b36fe4e749497685f87cb255438eaad
giftson solomon <giftsonsolomon@icloud.com>
2021-08-18 15:24:36 +0530

    we try to fix first

---
+++ pwd-creation/02.py
@@ -0,0 +1,16 @@
+# password creation
+#a={ "a", "b", "cdefghijklmnopqrstuvwxyz" }
+a={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y", "z"}
+b={"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"}
+c={"0","1","2","3","4","5","6","7","8","9"}
+d={"~","!","#","@","#","$","%","^","&","*","(",")","_","-","+","=","{","}","[","]",";",":"}
+
+
+u=list(a).pop()
+v=list(b).pop()
+w=list(c).pop()
+x=list(d).pop()
+
+
+
+print("Use the new PASSWORD: ",u,v,w,x, sep='')
---
+++ pwd-creation/03.py
@@ -0,0 +1,18 @@
+# password creation
+a={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y", "z"}
+b={"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"}
+c={"0","1","2","3","4","5","6","7","8","9"}
+d={"~","!","#","@","#","$","%","^","&","*","(",")","_","-","+","=","{","}","[","]",";",":"}
+
+
+u=list(a).pop()
+v=list(b).pop()
+w=list(c).pop()
+x=list(d).pop()
+i=list(c).pop()
+j=list(d).pop()
+k=list(b).pop()
+l=list(a).pop()
+
+
+print("Use the new PASSWORD: ",u,i,j,v,k,l,w,x, sep='')
